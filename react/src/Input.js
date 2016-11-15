89a9cfde064d2b384648ac49a7211e3a7d043a4b
vlad-saling <vladislavsaling@gmail.com>
2016-11-15 12:28:48 +0100

    moving react files and adding vanilla react test

---
+++ react/src/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Input extends React.Component {
+    render(){
+        return(
+            <input type="text" name="state you name" />
+        )
+    }
+}
+
+export default Input;
